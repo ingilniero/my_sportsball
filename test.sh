0081ae26a545bb923bccedda18da926927153aa2
Gilberto Villa <ingilniero@gmail.com>
2018-08-24 15:04:26 -0500

    Add test scripts for app component and main app

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+exit_code=0
+
+echo "*** Running container app specs"
+bundle
+bundle exec rake db:drop
+bundle exec rake environment db:create db:migrate
+RAILS_ENV=test bundle exec rake environment db:create db:migrate
+bundle exec rspec spec
+exit_code+=$?
+
+exit $exit_code
