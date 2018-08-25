934191db9276b67ac2093a9165ff4e81dc48fb11
Gilberto Villa <ingilniero@gmail.com>
2018-08-25 11:51:57 -0500

    Create predictor gem

---
+++ components/predictor/spec/predictor_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Predictor do
+  it "has a version number" do
+    expect(Predictor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
